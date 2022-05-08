3325ac7acd59359b69871588e6265cb6dd09d765
OPuusalu <opuusalu@gmail.com>
2022-05-08 21:42:18 +0300

    test

---
+++ Main/items_class.py
@@ -0,0 +1,7 @@
+class Items:
+
+    def __init__(self, ID, name, farmability, incredients):
+        self.ID = ID
+        self.name = name
+        self.farmability = farmability
+        self.incredients = incredients
