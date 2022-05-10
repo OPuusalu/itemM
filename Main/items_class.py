9db231ce69704d4791e8246754e19b1149b046e1
OPuusalu <opuusalu@gmail.com>
2022-05-10 13:41:22 +0300

    not has some amounts, but not sure it its 1.2x and 0.8x

--- Main/items_class.py
+++ Main/items_class.py
@@ -11,3 +11,7 @@ class Items:
 
     def ingredients_list(self):
         return self.ingredients
+    
+    # Return farmability of item
+    def get_farmability(self):
+        return self.farmability
